63bd4aab7a25df64507a421b92bc6e251900f26f
manuelito <mantinez@gmail.com>
2021-07-28 19:10:13 -0300

    agrego cositas

--- src/test/java/manu/pruebaelastic/PruebaelasticApplicationTests.java
+++ src/test/java/manu/pruebaelastic/PruebaelasticApplicationTests.java
@@ -1,7 +1,17 @@
 package manu.pruebaelastic;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import manu.pruebaelastic.model.Article;
+import manu.pruebaelastic.services.ArticleService;
 
 @SpringBootTest
 class PruebaelasticApplicationTests {
@@ -9,5 +19,24 @@ class PruebaelasticApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	// @Autowired
+	// @Qualifier("springDataJpaService")
+	// ArticleService articleService;
+
+	// protected ArticleService getService() {
+	// return articleService;
+	// }
+
+	// @BeforeEach
+	// public void setUp() {
+	// this.articleService = this.getService();
+	// }
+
+	// @Test
+	// public void testCreateArticle() {
+	// Article art = new Article("un articulo");
+	// assertNotNull(art.getId());
+	// }
 
 }
